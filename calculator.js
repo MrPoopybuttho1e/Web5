239ec4f7b5a26ad38b1a4abc4e22ffaa2aaeba73
MrPoopybuttho1e <91161758+MrPoopybuttho1e@users.noreply.github.com>
2021-12-01 20:40:51 +0300

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,18 @@
+function fun1() {
+    let f1 = document.getElementsByName("file1");
+    let f2 = document.getElementsByName("file2");
+    let r = document.getElementById("result");
+    var result = parseInt(f1[0].value) * parseInt(f2[0].value)
+    r.innerHTML = result;
+    let с = document.getElementById("btn1");
+    return false;
+}
+function fun2() {
+    let f1 = document.getElementsByName("file1");
+    let f2 = document.getElementsByName("file2");
+    let r = document.getElementById("result");
+    r.innerHTML = None;
+    f1.innerHTML = None;
+    f2.innerHTML = None;
+    return false;
+}
